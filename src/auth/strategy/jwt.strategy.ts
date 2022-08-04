d2a764690ac1b92ee5ef69307b1f5cc7984387b6
Mwawaka <kedskeds90@gmail.com>
2022-08-04 15:09:46 +0300

    Update:add typescript type

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -3,6 +3,7 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
+import { User } from '@prisma/client';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PrismaService } from '../../prisma/prisma.service';
 
@@ -16,7 +17,7 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
       secretOrKey: config.get<string>('JWT_SECRET_KEY'),
     });
   }
-  async validate(payload: { sub: number; email: string }) {
+  async validate(payload: { sub: number; email: string }): Promise<User> {
     const user = await this.prisma.user.findUnique({
       where: { id: payload.sub },
     });
